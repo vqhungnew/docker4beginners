d2b7f6c48b36482148e6162f2a006331d37ae0e6
Vu Quoc Hung <48969793+vqhungnew@users.noreply.github.com>
2025-01-01 20:00:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello NEW BEGINNERS for DOCKER COMPOSE </br>";
-echo "Hope all of you will be successfull in deploying a web page to Docker ";
+echo "Hello NEW BEGINNERS for DOCKER COMPOSE <br>";
+echo "Hope all of you will be successfull in deploying a web page to Docker<br> ";
 
 ?>
